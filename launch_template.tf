93fa342daf19575091a42cf59e75180625dbad3e
saad <mdsaadsardar2000@gmail.com>
2024-06-03 13:48:34 +0530

    cleaning process.

--- launch_template.tf
+++ launch_template.tf
@@ -34,30 +34,6 @@ resource "aws_launch_template" "example" {
     db_password = "12345678",
     db_endpoint = aws_rds_cluster.example.endpoint
   }))
-
-#   user_data = base64encode(<<EOF
-# #!/bin/bash
-# wget https://wordpress.org/latest.tar.gz
-# tar -xzf latest.tar.gz
-# sudo mv wordpress/* /var/www/html/
-# sudo chown -R ec2-user:ec2-user /var/www/html/
-# sudo find /var/www/html/ -type d -exec chmod 755 {} \;
-# sudo find /var/www/html/ -type f -exec chmod 644 {} \;
-# cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-#               # Replace database_name_here and username_here
-# sed -i 's/database_name_here/wordpress/g' /var/www/html/wp-config.php
-# sed -i 's/username_here/admin/g' /var/www/html/wp-config.php
-# sed -i 's/password_here/12345678/g' /var/www/html/wp-config.php
-# sed -i 's/localhost/example.cluster-czdmyhc4qqhe.ap-south-1.rds.amazonaws.com/g' /var/www/html/wp-config.php
-
-#               # Create health-check.php file
-# echo '<?php' > /var/www/html/health-check.php
-# echo '// health-check.php' >> /var/www/html/health-check.php
-# echo 'http_response_code(200);' >> /var/www/html/health-check.php
-# echo 'echo "Healthy";' >> /var/www/html/health-check.php
-# EOF
-#   )
 }
 
 
